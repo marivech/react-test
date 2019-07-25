ff4585a0cdf6dfbe7d12020d30dd8105808df0ed
marivech <maria.vechkanova@gmail.com>
2019-07-25 16:19:06 +0400

    change class-based component to function-based one

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-class App extends Component {
-  state = {
+const app = props => {
+  const [ personsState, setPersonsState ] = useState({
     persons: [
       {
         name: 'Murcheekee',
@@ -20,22 +20,23 @@ class App extends Component {
     ],
     name: 'Kek',
     info: 'top',
-  }
-  switchNameHandler = () => {
-    this.setState({
-      name: this.state.persons[1].name,
-      info: this.state.persons[1].info,
+  });
+
+  console.log(personsState)
+  const switchNameHandler = () => {
+    setPersonsState({
+      name: personsState.persons[1].name,
+      info: personsState.persons[1].info,
     })
   }
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a react app!</h1>
-        <button onClick={ this.switchNameHandler }>Switch name</button>
-        <Person name={ this.state.name } className="highlight" info={this.state.info}>And I am Myau</Person>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>Hi, I'm a react app!</h1>
+      <button onClick={ switchNameHandler }>Switch name</button>
+      <Person name={ personsState.name } className="highlight" info={personsState.info}>And I am Myau</Person>
+    </div>
+  );
 }
 
-export default App;
+
+export default app;
